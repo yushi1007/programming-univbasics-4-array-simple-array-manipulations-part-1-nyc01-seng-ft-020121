f9461498a39e9fec7cf41ba4b72ce62e766fbc77
Yu Shi <yushiys95@gmail.com>
2021-01-16 03:03:59 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_push(array, string)
+  array.push(string)
+end
+def using_unshift(array, string)
+  array.unshift(string)
+end
+def using_pop(array)
+  array.pop
+end
+def using_pop(array)
+  array = ["1", "2"]
+  2 = array.pop
+end
+def using_shift(array)
+  array.shift
+end
+def using_shift(array)
+  array = ["1", "2"]
+  2 = array.shift
+end
