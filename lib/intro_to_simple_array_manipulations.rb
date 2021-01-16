3b8fa3eb9ff77f972cbd3247fa2f0e9ac629aa97
Yu Shi <yushiys95@gmail.com>
2021-01-16 03:09:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,12 +7,12 @@ end
 def using_pop(array)
   array.pop
 end
-def using_pop(array)
+def pop_with_args(array)
   array.pop(2)
 end
 def using_shift(array)
   array.shift
 end
-def using_shift(array)
+def shift_with_args(array)
   array.shift(2)
 end
